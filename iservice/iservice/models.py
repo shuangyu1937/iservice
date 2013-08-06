3cc5d274e82880762c816eba624067dae0392f00
zouwei <zouweimobile@gmail.com>
2013-08-06 19:07:42 +0800

    数据模型修改

--- iservice/iservice/models.py
+++ iservice/iservice/models.py
@@ -2,21 +2,23 @@ __author__ = 'zouwei'
 from django.db import models
 
 
-class MessageStack:
+class MessageStack(models.Model):
     id = models.AutoField()
     title = models.CharField(verbose_name="标题", max_length=100)
-    source = models.CharField(verbose_name="来源", max_length=100)
     summary = models.CharField(verbose_name="摘要", max_length=300)
-    create_date = models.DateTimeField(verbose_name="消息时间")
+    author = models.DateTimeField(verbose_name="作者", max_length=50)
+    post_date = models.DateTimeField(verbose_name="消息时间")
+    url = models.CharField(verbose_name="地址", max_length=300)
+    source = models.CharField(verbose_name="来源", max_length=100)
 
 
-class UserReadMessage:
+class UserReadMessage(models.Model):
     id = models.AutoField()
     user_name = models.CharField(verbose_name="用户名", max_length=20)
     message_id = models.ForeignKey(MessageStack, verbose_name="已读消息")
 
 
-class ServiceKey:
+class ServiceKey(models.Model):
     id = models.AutoField()
     com = models.CharField(verbose_name="公司", max_length=50)
     app = models.CharField(verbose_name="应用", max_length=50)
--- iservice/iservice/settings.py
+++ iservice/iservice/settings.py
@@ -9,10 +9,15 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+import os
+ROOT_PATH = os.path.dirname(__file__)
+
+DB_FILE_PATH = ROOT_PATH+"/db/iservice.db"
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': DB_FILE_PATH,                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
